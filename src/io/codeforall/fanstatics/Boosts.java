a302310e47052573e8bfe93565eb62ab9d5dcf16
Camila <camila.lpessoa@gmail.com>
2024-10-14 19:13:38 +0100

    enum boost created

--- src/io/codeforall/fanstatics/Boosts.java
+++ src/io/codeforall/fanstatics/Boosts.java
@@ -2,6 +2,19 @@ package src.io.codeforall.fanstatics;
 
 public enum Boosts {
 
+    HEALTH_BOOST(20),
+    SPEED(2),
+    ARMOR(50),
+    BULLETS(5),
+    RELOADTIME(0.5);
+
+    private double value;
+
+    Boosts(double value){
+        this.value=value;
+    }
+
+
     // DEVELOP MEW BOOSTS TO BE DROPPED FROM ENEMIES
     // ENUMERATOR SHOULD CONTAIN AT LEAST 3 BOOSTS
     // IMPLEMENTATION IS NOT NEEDED BUT IS NICE
