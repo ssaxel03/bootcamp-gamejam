9d6c8664dcbf651f705abbea7630448157a527d1
Miguel Arnaud <miguelarnaud5@gmail.com>
2024-10-14 18:15:53 +0100

    enum guns created

--- src/io/codeforall/fanstatics/Guns.java
+++ src/io/codeforall/fanstatics/Guns.java
@@ -1,6 +1,25 @@
 package src.io.codeforall.fanstatics;
 
 public enum Guns {
+    PISTOL(5,800),
+    RIFLE(10,100),
+    SHOTGUN(15,400);
+
+    private int damage;
+    private int delayMs;
+
+    Guns(int damage, int delayMs){
+        this.damage = damage;
+        this.delayMs = delayMs;
+    }
+
+    public int getDamage(){
+        return this.damage;
+    }
+
+    public long getDelayMs(){
+        return this.delayMs;
+    }
 
     // DEVELOP NEW GUNS TO BE BOUGHT BY THE PLAYER
     // AT LEAST 3 GUNS ARE NEEDED
